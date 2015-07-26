05626b387e236abcdd540deb9d3ccf9bb6dd3b32
Christine Dodrill <xena@yolo-swag.com>
2015-07-25 22:30:02 -0700

    main: add package documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Command xeserv.us is the server backend for http://xeserv.us
+
+This is a very ephemeral and stateless web application. Future
+improvments will be careful not to jeopardize this simplicity.
+*/
+package main
